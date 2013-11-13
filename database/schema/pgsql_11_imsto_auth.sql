fd12d46da4fd0a74d700ba1b75c1957de37c9bb8
Liut <Eagle.L@gmail.com>
2013-11-13 18:28:00 +0800

    add ticket schema

---
+++ database/schema/pgsql_11_imsto_auth.sql
@@ -0,0 +1,23 @@
+
+-- 访问授权相关
+BEGIN;
+
+set search_path = imsto, public;
+
+CREATE TABLE camera_ticket (
+	id serial,
+	app_id int NOT NULL DEFAULT 0,
+	author int NOT NULL ,
+	prompt varchar(255) NOT NULL DEFAULT '',
+	url_prefix varchar(112) NOT NULL DEFAULT '',
+	img_id varchar (44) NOT NULL DEFAULT '',
+	img_path varchar(65) NOT NULL DEFAULT '',
+	img_meta hstore,
+	uploaded boolean NOT NULL DEFAULT false,
+	created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
+) WITHOUT OIDS;
+
+
+END;
