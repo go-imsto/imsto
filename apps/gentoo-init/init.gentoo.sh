b5acce0b969b98c4bbf8a612ee4361431f6d9095
Liut <Eagle.L@gmail.com>
2013-11-28 00:50:16 +0800

    fix gentoo.init

--- apps/gentoo-init/init.gentoo.sh
+++ apps/gentoo-init/init.gentoo.sh
@@ -2,10 +2,12 @@
 # Copyright 1999-2013 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
+# copy this file into /etc/init.d/ and named 'imsto'
+
 IMSTO_APP_ROOT=${IMSTO_APP_ROOT:-/opt/imsto}
 IMSTO_API_0_SALT=${IMSTO_API_0_SALT:-imstosalt}
 ACT=${SVCNAME#*.}
-if [ -z "${ACT}" -o ${ACT} != "stage"]; then
+if [ -z "${ACT}" -o ${ACT} != "stage" ]; then
 	ACT="tiring"
 fi
 
@@ -20,8 +22,13 @@ depend() {
 	before nginx
 }
 
+start_pre() {
+	[ -d /var/run/imsto ] || mkdir /var/run/imsto && chown nobody /var/run/imsto
+	[ -d /var/log/imsto ] || mkdir /var/log/imsto && chown nobody /var/log/imsto
+}
+
 start() {
-	ebegin "Starting ${SVCNAME}"
+	ebegin "Starting imsto.${ACT}"
 	start-stop-daemon --background --start --exec \
 	env IMSTO_API_0_SALT=${IMSTO_API_0_SALT} \
 	${IM_BIN} \
@@ -32,7 +39,7 @@ start() {
 }
 
 stop() {
-    ebegin "Stopping ${SVCNAME}"
+    ebegin "Stopping imsto.${ACT}"
     start-stop-daemon --stop --exec ${IM_BIN} \
     --pidfile ${IM_PID}
     eend $?
