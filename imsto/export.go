bb3065dcd7fb96157c96e6612543e3b7cf3ab8cc
Liut <Eagle.L@gmail.com>
2013-12-06 17:44:27 +0800

    add export one entry if id specialed

--- imsto/export.go
+++ imsto/export.go
@@ -20,6 +20,7 @@ export to local system
 var (
 	eroof  string
 	edir   string
+	eid    string
 	etotal int
 	elimit int
 )
@@ -32,6 +33,7 @@ func init() {
 	cmdExport.Run = runExport
 	cmdExport.Flag.StringVar(&eroof, "s", "", "config section name")
 	cmdExport.Flag.StringVar(&edir, "o", "", "a local direcotry to export into.")
+	cmdExport.Flag.StringVar(&eid, "id", "", "only export a special id.")
 	cmdExport.Flag.IntVar(&etotal, "total", 0, "export total count.")
 	cmdExport.Flag.IntVar(&elimit, "limit", 10, "export total count.")
 }
@@ -43,6 +45,19 @@ func runExport(args []string) bool {
 	}
 
 	mw := storage.NewMetaWrapper(eroof)
+	if eid != "" {
+		id, err := storage.NewEntryId(eid)
+		if err != nil {
+			fmt.Printf("error id: %s, %s", eid, err)
+			return false
+		}
+		entry, err := mw.GetEntry(*id)
+		if err != nil {
+			fmt.Printf("get entry error: %s", err)
+			return false
+		}
+		return _save_export(entry, edir)
+	}
 	total, err := mw.Count()
 	if err != nil {
 		fmt.Println(err)
@@ -71,23 +86,30 @@ func runExport(args []string) bool {
 		}
 
 		for _, entry := range a {
-			data, err := storage.FetchBlob(entry, eroof)
-			if err != nil {
-				fmt.Println(err)
+			if !_save_export(entry, edir) {
 				return false
-			}
-			filename := path.Join(edir, entry.Path)
-			fmt.Printf("save to: %s ", filename)
-			err = storage.SaveFile(filename, data)
-			if err != nil {
-				fmt.Println(err)
-				return false
-			} else {
-				fmt.Print("ok\n")
 			}
 		}
 		skip += limit
 	}
 
+	return true
+}
+
+func _save_export(entry *storage.Entry, edir string) bool {
+	data, err := storage.FetchBlob(entry, eroof)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	filename := path.Join(edir, entry.Path)
+	fmt.Printf("save to: %s ", filename)
+	err = storage.SaveFile(filename, data)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	} else {
+		fmt.Print("ok\n")
+	}
 	return true
 }
